dd7cfe68b389c1ea98eba2c47fcfc8f00570d9b6
chinmaypant21 <pant.chinu.2@gmail.com>
2024-01-16 23:29:29 +0530

    Crash fix #2

--- src/component/InputComponent.tsx
+++ src/component/InputComponent.tsx
@@ -14,11 +14,6 @@ const InputComponent = () => {
         setInputSearchValue(e.target.value)
     }
 
-    function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {
-        e.stopPropagation();
-        setBackspaceCount(0);
-    }
-
     function handleChipRemove(chipId: number){
         return () => {
             setPickedUsers(prevUsers => (
